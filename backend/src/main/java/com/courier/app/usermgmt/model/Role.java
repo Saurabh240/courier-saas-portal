8d3ad614b94d2e125265b22d01924ccdf2c5723d
Saurabh240 <saurabh58833@gmail.com>
2025-06-10 20:33:04 +0530

    updated role

--- backend/src/main/java/com/courier/app/usermgmt/model/Role.java
+++ backend/src/main/java/com/courier/app/usermgmt/model/Role.java
@@ -1,7 +1,7 @@
 package com.courier.app.usermgmt.model;
 
 public enum Role {
-    ADMIN, STAFF, PARTNER, CUSTOMER;
+    ADMIN, STAFF, DELIVERY_PARTNER, CUSTOMER;
 
     public static Role fromString(String value) {
         return Role.valueOf(value.trim().toUpperCase());
