de9bbc6953434a6fbccd56cd3a6b8021dd41f916
SaiTech01 <111438819+SaiTech01@users.noreply.github.com>
2025-06-29 21:11:00 +0530

    Enum done

--- backend/src/main/java/com/courier/app/orders/model/OrderRequest.java
+++ backend/src/main/java/com/courier/app/orders/model/OrderRequest.java
@@ -1,4 +1,4 @@
 package com.courier.app.orders.model;
 
-public record OrderRequest(String customerEmail, String senderName, String receiverName, String pickupAddress, String deliveryAddress,String packageType,double packageWeightKg,int packageLengthCm,int packageWidthCm,int packageHeightCm,
-                           String pickupPhone,String deliveryPhone,String pickupDate,String pickupTimeWindow,String specialInstructions,String paymentMode,double declaredValue,Boolean isFragile,String deliveryType) {}
+public record OrderRequest(String customerEmail, String senderName, String receiverName, String pickupAddress, String deliveryAddress, PackageType packageType,double packageWeightKg,int packageLengthCm,int packageWidthCm,int packageHeightCm,
+                           String pickupPhone,String deliveryPhone,String pickupDate,String pickupTimeWindow,String specialInstructions,PaymentMode  paymentMode,double declaredValue,Boolean isFragile,  DeliveryType deliveryType) {}
--- backend/src/main/java/com/courier/app/orders/model/OrderStatus.java
+++ backend/src/main/java/com/courier/app/orders/model/OrderStatus.java
@@ -3,3 +3,7 @@ package com.courier.app.orders.model;
 public enum OrderStatus {
     CREATED, PICKED_UP, IN_TRANSIT, DELIVERED, CANCELLED
 }
+
+
+
+
