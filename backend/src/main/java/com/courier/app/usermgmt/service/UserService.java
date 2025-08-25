ed19869b983c16ae6e4efd8ab1cfd1399da108ae
Nandini016 <nandinis2216@gmail.com>
2025-08-25 17:01:53 +0530

    added filter in list of user based on role

--- backend/src/main/java/com/courier/app/usermgmt/service/UserService.java
+++ backend/src/main/java/com/courier/app/usermgmt/service/UserService.java
@@ -2,6 +2,7 @@ package com.courier.app.usermgmt.service;
 
 import com.courier.app.usermgmt.dto.RegisterRequest;
 import com.courier.app.usermgmt.dto.UserResponse;
+import com.courier.app.usermgmt.model.Role;
 import com.courier.app.usermgmt.model.User;
 import com.courier.app.usermgmt.repository.UserRepository;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -25,7 +26,7 @@ public class UserService {
 
     public UserResponse register(RegisterRequest req) {
         String hashed = encoder.encode(req.getPassword());
-        User user = new User(req.getName(), req.getEmail(), hashed, req.getPhoneNo(), req.getRole());
+        User user = new User(req.getName(), req.getEmail(), hashed, req.getPhoneNo(), req.getRole(),req.getVerified());
         User saved = null;
         try {
             saved = repo.save(user);
@@ -33,12 +34,22 @@ public class UserService {
             throw new DataIntegrityViolationException("users_email_key", ex);
         }
 
-        return new UserResponse(saved.getId(), saved.getName(), saved.getEmail(), saved.getPhoneNo(), saved.getRole());
+        return new UserResponse(saved.getId(), saved.getName(), saved.getEmail(), saved.getPhoneNo(), saved.getRole(),saved.getVerified());
     }
 
-    public List<UserResponse> listUsers() {
+    public List<UserResponse> listUsers(String role, Boolean verified) {
         return repo.findAll().stream()
-                .map(u -> new UserResponse(u.getId(), u.getName(), u.getEmail(), u.getPhoneNo(),u.getRole()))
+                .filter(u -> role == null || u.getRole() == Role.valueOf(role.toUpperCase()))
+                .filter(u -> verified == null || u.getVerified() == verified)
+                .map(u -> new UserResponse(
+                        u.getId(),
+                        u.getName(),
+                        u.getEmail(),
+                        u.getPhoneNo(),
+                        u.getRole(),
+                        u.getVerified()
+                ))
                 .collect(Collectors.toList());
     }
+
 }
