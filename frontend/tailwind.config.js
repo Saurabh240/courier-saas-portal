0e75884a08dafd8aea6ead3ee5e41f67a9f63966
chinky23-tech <chinky0330@gmail.com>
2026-06-01 21:56:42 +0530

    tailwind css version 3 files fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./index.html",
-  "./src/**/*.{js,jsx,ts,tsx}",
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
